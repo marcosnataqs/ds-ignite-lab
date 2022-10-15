a02521ced7141f3e7bbca93ee9d50dfe6f4d7fb1
marcosnataqs <marcosnataqs@gmail.com>
2022-10-15 17:20:38 -0300

    first commit

---
+++ src/components/Text.tsx
@@ -0,0 +1,24 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export function Text({ size = 'md', children, asChild }: TextProps) {
+  const Comp = asChild ? Slot : 'span';
+
+  return (
+    <Comp
+      className={clsx('text-gray-100 font-sans', {
+        'text-sm': size === 'sm',
+        'text-md': size === 'md',
+        'text-lg': size === 'lg',
+      })}
+    >
+      {children}
+    </Comp>
+  );
+}
